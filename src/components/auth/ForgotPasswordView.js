c67e35bf4c356aa7a87ee93df449c9a0b1f07aea
githuberh <70492874+githuberh@users.noreply.github.com>
2020-10-04 13:48:42 +0300

    Add files via upload

---
+++ src/components/auth/ForgotPasswordView.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useInput } from "../../hooks/useInput"
+import { client } from "../../helpers/httpHelpers"
+import alertify from "alertifyjs"
+
+function ResetPassword({ changeToLogin }) {
+    const [inputs, setInputs] = useInput({ email: "", hasError: false, errorMessage: "" })
+
+    const onUserClick = (e) => {
+        e.preventDefault()
+        const body = {
+            email: inputs.email
+        }
+        client("auth/forgotpassword", { body })
+            .then(data => {
+                if (!data.status) {
+                    alertify.error(data.message)
+                    return
+                }
+                alertify.success(data.message)
+            })
+    }
+
+    return (
+        <div>
+            <form onSubmit={onUserClick}>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input name="email" type="email" className="form-control" placeholder="Email adresi" onChange={setInputs} />
+                </div>
+                <button type="submit" className="btn btn-success btn-lg btn-block">Sıfırlama Maili Gönder</button>
+            </form>
+            <p className="authFooterMsg"
+                onClick={changeToLogin}><b>Şifreni hatırlıyor musun? O zaman giriş yap</b></p>
+        </div>
+    )
+}
+
+export default ResetPassword
---
+++ src/helpers/config.js
@@ -0,0 +1,7 @@
+export default {
+    apiVersion: "v1",
+    apiUrl: "http://localhost:5000",
+    cloudinaryUploadURL: "https://api.cloudinary.com/v1_1/dleaeduof/upload",
+    cloudinaryPreset: "w6xkchlf",
+    cloudinaryVideosURL: "https://res.cloudinary.com/dleaeduof/video/upload/v1601680063/"
+}
